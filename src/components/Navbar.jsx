c9f4d48d339b8ecef0152759ff5197f951ae487a
lesley <lesleybanadzem2017@gmail.com>
2019-10-08 09:28:54 -0700

    Finish the darkmode custom hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import { useDarkMode } from '../hooks/useDarkMode';
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleMode = e => {
-    e.preventDefault();
-    setDarkMode(!darkMode);
-  };
+  const [darkMode, setDarkMode] = useDarkMode();
+  
+  useEffect(() => {
+    console.log(darkMode)
+  }, [darkMode])
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
       <div className="dark-mode__toggle">
         <div
-          onClick={toggleMode}
+          onClick={()=>{setDarkMode(!darkMode)}}
           className={darkMode ? 'toggle toggled' : 'toggle'}
         />
       </div>
--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const App = () => {
   const [coinData, setCoinData] = useState([]);
 
   useEffect(() => {
+    
     fetchCoinData();
   }, []);
 
